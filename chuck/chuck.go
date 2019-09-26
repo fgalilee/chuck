4becd73b86d5fcd56abc5cccaed14ec28dfbc654
Francois Galilee <galilee@google.com>
2019-09-26 16:00:05 +1000

    Chuck 1.1.0

--- chuck/chuck.go
+++ chuck/chuck.go
@@ -1,5 +1,11 @@
 package chuck
 
+import (
+	"fmt"
+
+	"github.com/fgalilee/bob/bob"
+)
+
 func Chuck() string {
-	return "Chuck 1.0.0"
+	return fmt.Sprintf("Chuck 1.1.0: <%s>", bob.Bob())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fgalilee/chuck
 
 go 1.13
+
+require github.com/fgalilee/bob v1.0.0 // indirect
